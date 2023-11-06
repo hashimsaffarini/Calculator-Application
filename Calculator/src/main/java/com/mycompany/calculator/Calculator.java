5075c22c65f1456d3bde530e8e3e4e4990888b67
hashimsaffarini <hashimsaffarini044@gmail.com>
2023-11-06 16:58:46 +0200

    First App

---
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+    }
+}
